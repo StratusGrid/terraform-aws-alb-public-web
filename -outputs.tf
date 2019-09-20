95b7d261f14da35704ab35833df22e6df285e99c
Chris Hurst <chris.hurst@genesisfunction.com>
2019-09-20 17:06:15 -0400

    added output

--- -outputs.tf
+++ -outputs.tf
@@ -2,3 +2,8 @@ output "sg_alb_targets_id" {
   description = "ID of security group which ALB targets are added to for ALB to have access"
   value       = aws_security_group.this_targets.id
 }
+
+output "alb_dns_name" {
+  description = "DNS Name of ALB"
+  value       = aws_lb.this.dns_name
+}
